cd7e255789365dbf2cc7208dcd082602c7f5a860
TimoWieme <1004149@hr.nl>
2021-10-31 15:28:23 +0100

    Can now search in description and title

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -16,7 +16,8 @@ class SearchController extends Controller
         // Request the word you are trying to search
         $data = request()->validate(['search' => ['required', 'string']]);
         // Search on a word that is similair to the title of the serie
-        $series = Serie::where('title', 'like', '%' . $data['search'] . '%')->get();
+        $series = Serie::where('title', 'like', '%' . $data['search'] . '%')->orWhere('description', 'like', '%' . $data['search'] . '%')->get();
+
 
 //        $series = Genre::where('genre', 'like', '%' . $data['filter'])->get();
 
